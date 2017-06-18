cf4934e628f351db5472e33650c3853ddd2fa5ee
Zack Rauen <rauenzi@outlook.com>
2017-06-18 19:10:59 -0400

    Bug fixes

--- bot.py
+++ bot.py
@@ -3,10 +3,6 @@
 import asyncio
 import discordbot
 
-description = """
-BoiBot, for when you need to yell boi but with pictures.
-"""
-
 try:
     import uvloop
 except ImportError:
@@ -14,15 +10,11 @@ except ImportError:
 else:
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-cogs = [
-    'cogs.boibot'
-]
 
-prefix = '/'
-bot = discordbot.DiscordBot(command_prefix=prefix, description=description, pm_help=False, help_attrs=dict(hidden=True))
+bot = discordbot.DiscordBot()
 
 
 if __name__ == '__main__':
-    bot.load_cogs(cogs)
+    bot.load_cogs()
     bot.run()
 

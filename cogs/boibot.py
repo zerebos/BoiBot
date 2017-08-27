e7d841fdcb82e59875dd52cc2058d5d532c606e1
Zack Rauen <rauenzi@outlook.com>
2017-08-26 22:30:18 -0400

    Fix addboi command and update token

--- cogs/boibot.py
+++ cogs/boibot.py
@@ -54,6 +54,10 @@ class BoiBot:
                 if re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + image + r'\..*', file):
                     image = file
                     break
+        os.remove(image)
+
+        await self.bot.responses.success(title="Boi Removed",
+                                         message="Your boi \"" + image + "\" has been removed.")
 
     @staticmethod
     def _boi_list():
@@ -114,7 +118,7 @@ class BoiBot:
             await self.bot.responses.failure(message="Something went wrong ¯\_(ツ)_/¯")
 
 
-    async def downloadImage(url, folder, name, loop, chunkSize=20):
+    async def downloadImage(self, url, folder, name, loop, chunkSize=20):
         result = {'canAccessURL': False, 'isImage': False, 'fileSaved': False}
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -123,27 +127,26 @@ class BoiBot:
             'Accept-Language': 'en-US,en;q=0.8',
             'Connection': 'keep-alive'}
         async with aiohttp.ClientSession(loop=loop) as session:
-            with aiohttp.Timeout(10, loop=session.loop):
-                async with session.get(url, headers=headers) as response:
-                    content_type = response.headers['content-type']
-                    if response.status == 200:
-                        result['canAccessURL'] = True
-                    if "image" in content_type:
-                        result['isImage'] = True
-                    if not result['canAccessURL'] or not result['isImage']:
-                        return result
-                    extension = mimetypes.guess_extension(content_type)
-                    if extension == '.jpe':
-                        extension = '.jpg'
-
-                    with open(folder + "/" + name + extension, 'wb') as fd:
-                        while True:
-                            chunk = await response.content.read(chunkSize)
-                            if not chunk:
-                                break
-                            fd.write(chunk)
-                    result['fileSaved'] = True
+            async with session.get(url, headers=headers, timeout=10) as response:
+                content_type = response.headers['content-type']
+                if response.status == 200:
+                    result['canAccessURL'] = True
+                if "image" in content_type:
+                    result['isImage'] = True
+                if not result['canAccessURL'] or not result['isImage']:
                     return result
+                extension = mimetypes.guess_extension(content_type)
+                if extension == '.jpe':
+                    extension = '.jpg'
+
+                with open(folder + "/" + name + extension, 'wb') as fd:
+                    while True:
+                        chunk = await response.content.read(chunkSize)
+                        if not chunk:
+                            break
+                        fd.write(chunk)
+                result['fileSaved'] = True
+                return result
 
 
 def setup(bot):

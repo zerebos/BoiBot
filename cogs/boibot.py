6b022712bd03e052f17332076b8ece3c3ff35bc3
Zack Rauen <rauenzi@outlook.com>
2017-10-02 02:41:46 -0400

    Move to an approval-based system to prevent unsolicited images.

--- cogs/boibot.py
+++ cogs/boibot.py
@@ -5,6 +5,7 @@ import mimetypes
 import glob, aiohttp
 
 BOI_FOLDER = "boi"
+SUBMISSION_FOLDER = "submissions"
 
 class BoiBot:
     """For all your BOI needs."""
@@ -12,6 +13,8 @@ class BoiBot:
     def __init__(self, bot):
         self.bot = bot
         self.config = discordbot.Config('boi.json', loop=bot.loop, directory="data")
+        os.makedirs(BOI_FOLDER, exist_ok=True)
+        os.makedirs(SUBMISSION_FOLDER, exist_ok=True)
 
     @discordbot.commands.command(pass_context=True)
     async def boi(self, ctx, image=""):
@@ -38,27 +41,6 @@ class BoiBot:
 
         await self.bot.upload(image)
 
-    @discordbot.commands.command(pass_context=True)
-    @discordbot.checks.is_owner()
-    async def deleteboi(self, ctx, image=""):
-        """Deletes a specific Boi."""
-
-        if ctx.message.server is not None:
-            if ctx.message.channel.permissions_for(ctx.message.server.me).manage_messages:
-                await self.bot.delete_message(ctx.message)
-
-        image = image.lower()
-
-        if image != "":
-            for file in glob.glob(BOI_FOLDER+'/*'):
-                if re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + image + r'\..*', file):
-                    image = file
-                    break
-        os.remove(image)
-
-        await self.bot.responses.success(title="Boi Removed",
-                                         message="Your boi \"" + image + "\" has been removed.")
-
     @staticmethod
     def _boi_list():
         # re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + image + r'\..*', file)
@@ -66,13 +48,13 @@ class BoiBot:
         names.sort()
         return '      '.join(names)
 
-    @discordbot.commands.command()
-    async def listbois(self):
+    @discordbot.commands.command(aliases=['listbois'])
+    async def bois(self):
         """Prints a list of all the available Boi memes."""
         await self.bot.responses.basic(title="Current Bois:", message=BoiBot._boi_list())
 
-    @discordbot.commands.command(pass_context=True)
-    async def addboi(self, ctx, name=None, link=None):
+    @discordbot.commands.command(pass_context=True, aliases=['addboi'])
+    async def add(self, ctx, name=None, link=None):
         """Either upload an image, or provide URL and then filename
 
         When uploading a file, "link" should be the filename and name is not needed.
@@ -80,10 +62,10 @@ class BoiBot:
 
         **Usage Examples:**
         For Upload:
-        {prefix}addboi [filename]
+        {prefix}addboi [name]
 
         For URL:
-        {prefix}addboi [filename] [url]
+        {prefix}addboi [name] [url]
         """
 
         if name is None:
@@ -100,22 +82,105 @@ class BoiBot:
             await self.bot.responses.failure(message="You did not provide a URL or upload an image.")
             return
 
-        for file in glob.glob(BOI_FOLDER+"/*"):
+        for file in glob.glob(SUBMISSION_FOLDER+"/*"):
             file = file.replace('\\', "/")
-            if re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + name + r'\..*', file):
+            if re.match(r'\.?/?\\?'+SUBMISSION_FOLDER+r'/?\\?' + name + r'\..*', file):
                 await self.bot.responses.failure(message="The boi \"{}\" already exists!".format(name))
                 return
 
-        result = await self.downloadImage(url, BOI_FOLDER, name, self.bot.loop)
+        result = await self.downloadImage(url, SUBMISSION_FOLDER, name, self.bot.loop)
 
         if not result['canAccessURL']:
             await self.bot.responses.failure(message="The URL provided was invalid.")
         elif not result['isImage']:
             await self.bot.responses.failure(message="The URL was not to a direct image.")
         elif result['fileSaved']:
-            await self.bot.responses.success(title="Boi Added", message="Your boi \"" + name + "\" has been added successfully.", thumbnail=url)
+            await self.bot.responses.success(title="Boi Submitted", message="Your boi \"" + name + "\" has been submitted for approval.", thumbnail=url)
+            await self.bot.responses.basic(destination=await self.bot.get_user_info(self.bot.config.get('meta', {}).get('owner', "249746236008169473")),
+                                           author=str(ctx.message.author), author_img=ctx.message.author.avatar_url,
+                                           title="Submission Received", message="A new boi submission \"" + name + "\" has been received.")
         else:
             await self.bot.responses.failure(message="Something went wrong ¯\_(ツ)_/¯")
+
+    @discordbot.commands.command(pass_context=True, aliases=['delboi', 'deleteboi'])
+    @discordbot.checks.is_owner()
+    async def delete(self, ctx, image):
+        """Deletes a specific Boi."""
+
+        image = image.lower()
+
+        for file in glob.glob(BOI_FOLDER+'/*'):
+            if re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + image + r'\..*', file):
+                os.remove(file)
+                await self.bot.responses.success(title="Boi Removed",
+                                                 message="Your boi \"" + image + "\" has been removed.")
+                return
+
+        await self.bot.responses.failure(message="The boi \"" + image + "\" does not exist.")
+
+    @staticmethod
+    def _submission_list():
+        # re.match(r'\.?/?\\?'+BOI_FOLDER+r'/?\\?' + image + r'\..*', file)
+        names = [re.sub(r'\.?/?\\?'+SUBMISSION_FOLDER+r'/?\\?'+r'(.*)\..*', r'\1', l) for l in glob.glob(SUBMISSION_FOLDER+"/*")]
+        names.sort()
+        return '      '.join(names)
+
+    @discordbot.commands.command()
+    @discordbot.checks.is_owner()
+    async def submissions(self):
+        """Prints a list of all the Boi submissions."""
+        await self.bot.responses.basic(title="Current Submissions:", message=BoiBot._submission_list())
+
+        # os.rename("path/to/current/file.foo", "path/to/new/desination/for/file.foo")
+
+    @discordbot.commands.command(pass_context=True)
+    @discordbot.checks.is_owner()
+    async def approve(self, ctx, image, new_name=""):
+        """Approve a submission to the working list."""
+
+        image = image.lower()
+
+        for file in glob.glob(SUBMISSION_FOLDER+'/*'):
+            if re.match(r'\.?/?\\?'+SUBMISSION_FOLDER+r'/?\\?' + image + r'\..*', file):
+                new_name = image if new_name == "" else new_name
+                os.rename(file, BOI_FOLDER + '/' + new_name + '.' + file.split('.')[-1])
+
+                await self.bot.responses.success(title="Submission Approved",
+                                                 message="The submission \"" + new_name + "\" has been approved.")
+                return
+
+        await self.bot.responses.failure(message="The submission \"" + image + "\" does not exist.")
+
+
+    @discordbot.commands.command(pass_context=True, aliases=['subs'])
+    @discordbot.checks.is_owner()
+    async def view(self, ctx, image):
+        """View a submission."""
+
+        image = image.lower()
+
+        for file in glob.glob(SUBMISSION_FOLDER+'/*'):
+            if re.match(r'\.?/?\\?'+SUBMISSION_FOLDER+r'/?\\?' + image + r'\..*', file):
+                await self.bot.upload(file)
+                return
+
+        await self.bot.responses.failure(message="The submission \"" + image + "\" does not exist.")
+
+    @discordbot.commands.command(pass_context=True)
+    @discordbot.checks.is_owner()
+    async def remove(self, ctx, image):
+        """Deletes a specific submission."""
+
+        image = image.lower()
+
+        for file in glob.glob(SUBMISSION_FOLDER + '/*'):
+            if re.match(r'\.?/?\\?' + SUBMISSION_FOLDER + r'/?\\?' + image + r'\..*', file):
+                os.remove(file)
+                await self.bot.responses.success(title="Submission Removed",
+                                                 message="The submission \"" + image + "\" has been removed.")
+                return
+
+        await self.bot.responses.failure(message="The submission \"" + image + "\" does not exist.")
 
 
     async def downloadImage(self, url, folder, name, loop, chunkSize=20):
